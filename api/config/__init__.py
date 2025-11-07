ac11ef9740942224c99c3b8d31907fe132d11fa1
Kainom <eleuteriokaina@gmail.com>
2025-11-07 09:59:34 -0300

    Try resolve the Event loop close

--- api/config/__init__.py
+++ api/config/__init__.py
@@ -1,1 +1,5 @@
-from .database import collection_notes, collection_tags, collection_tasks
+from .database import get_collection_notes, get_collection_tags, get_collection_tasks
+
+collection_notes = get_collection_notes
+collection_tags = get_collection_tags
+collection_tasks = get_collection_tasks
